fab5cd4952e8fa62b9fe2be12ecd6df8c9f9a775
IraKolbasina <irenLev@yandex.ru>
2023-05-16 07:25:07 +0300

    Настройка build.gradle и ветки testng c покрытием 100%

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -4,13 +4,15 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.Assertion;
 
+import static org.testng.Assert.*;
+
 public class CashbackHackServiceTest {
            @Test
         void remain() {
             int amount = 1900;
-            int expexted = 100;
+            int expeсted = 100;
             CashbackHackService service = new CashbackHackService();
             int actual = service.remain(amount);
-            Assert.assertEquals(actual, expexted);
+            assertEquals(actual, expeсted);
         }
     }
