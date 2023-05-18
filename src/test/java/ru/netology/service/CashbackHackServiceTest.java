18d2a94cf06457de596a4752b84b19bc1eb16a7d
IraKolbasina <irenLev@yandex.ru>
2023-05-18 07:26:49 +0300

    Настройка build.gradl, ошибка в тестировании

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,15 +1,22 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class CashbackHackServiceTest {
 
-class CashbackHackServiceTest {
-
-    @Test
-    void remain() {
+    @org.junit.Test
+    public void remainTestWithCB() {
         int amount = 1900;
         int expexted = 100;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(expexted, actual);
+    }
+
+    @org.junit.Test
+    public void remainTestWithoutCB() {
+        int amount = 2000;
+        int expexted = 0;
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
         assertEquals(expexted, actual);
